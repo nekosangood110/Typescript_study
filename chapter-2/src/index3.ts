2e7103757ef04d02d8de241ab0154949d30987fd
LAPTOP-SAHND00I\asuha <asuha2314@gmail.com>
2021-02-10 00:31:44 +0900

    アクセサメソッド

--- chapter-2/src/index3.ts
+++ chapter-2/src/index3.ts
@@ -1,14 +1,16 @@
 //Class
-//constructor インスタンス化する際のクラスに引数を渡す　コンストラクタの仮引数で受け取れる
+//constructor newでインスタンス化する際　()内をコンストラクタが受け取れる
+//特別なメソッド
 
 class Animal {
   age: number = 10;
-  private _legs = 4;
+  private _legs = 0;
   //ここのプライベートのはアクセス出来ないのでアクセサメソッドを使用
 
   constructor(public isCry: boolean) {
-    //   this.isCry = isCry;　Tsはアクセス修飾子(public)があるので
-    //プロパティに初期化は必要なし=自動的に取り込むことができることができる
+    //   this.isCry = isCry;　Tsはアクセス修飾子(public)があると
+    //プロパティに初期化は必要なし=cunstructorに引数が来ること伝えなくていい
+    //ここでいうthisはAnimal Class
   }
 
   cry(): void {
@@ -16,15 +18,19 @@ class Animal {
       alert(`age: ${this.age}`);
     }
   }
-  //アクセサメソッド(getter, setter)
+  //アクセサメソッド(getter, setter) class内に書く　privateにアクセスするもの
+  //getter読み取り専用　setterは書き込み専用
 
-  //比較(検証)すると安全
+
+  //privateなのにgetter書くだけでアクセス出来たら意味がないのでif文で比較してあげる
   get legs() {
     if (this._legs > 2) {
       return this._legs;
     }
+    return NaN;
+    //number型
   }
-  //設定するとき
+  //設定するとき(setter)
   set legs(value: number) {
       if (value > 1) {
           this._legs = value;
@@ -38,6 +44,7 @@ class Animal {
 
 //インスタンス化
 //new Animal() カッコ内はconstructorの引数にわたる
+//変数dogにAnimal型のインスタンスをいれてる　= let dogはAnimal型となる
 let dog = new Animal(true);
 dog.cry();
 
